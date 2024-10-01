66363c39d7124bdfc03263a5ea7fa8fd57da345a
vineshtv <tv.vinesh@gmail.com>
2024-10-01 21:28:38 +1000

    A binary search tree implementation with some unittests

---
+++ binarySearchTree/binarySearchTree_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"math/rand/v2"
+	"testing"
+)
+
+func TestBstCreation(t *testing.T) {
+	bst := &BinarySearchTree{root: nil}
+	if bst.root != nil {
+		t.Fatal("Bst root is not nil")
+	}
+}
+
+func Test100BstInserts(t *testing.T) {
+	bst := &BinarySearchTree{root: nil}
+
+	for i := 0; i < 100; i++ {
+		bst.insert(rand.IntN(100))
+	}
+}
+
+func Test100RandomActions(t *testing.T) {
+	bst := &BinarySearchTree{root: nil}
+
+	for i := 0; i < 10000; i++ {
+		rand_choice := rand.IntN(2)
+		if rand_choice == 0 {
+			bst.insert(rand.IntN(1000))
+		} else {
+			bst.delete(rand.IntN(1000))
+		}
+	}
+	bst.inOrderTraverse()
+}
